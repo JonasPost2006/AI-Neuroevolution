b8c18cbab0e807a661e9e1e487df788c797236ce
JonasPost2006 <43406465+JonasPost2006@users.noreply.github.com>
2023-04-04 23:43:48 +0200

    Begin render raytracing

---
+++ Raycasting met rendering/sketch.js
@@ -0,0 +1,36 @@
+let walls = [];
+let ray;
+let particle;
+
+const scene = [];
+
+function setup(){
+    createCanvas(400, 400);
+    for(let i = 0; i < 5; i++){
+        let x1 = random(width);
+        let x2 = random(width);
+        let y1 = random(height);
+        let y2 = random(height);
+        walls[i] = new Boundary(x1, y1, x2, y2);
+    }
+    particle = new Particle();
+}
+
+function draw(){
+    background(0);
+    for(let wall of walls){
+        wall.show();
+    }
+    particle.update(mouseX, mouseY);
+    particle.show();
+    scene = particle.look(walls);
+
+    // ray.show();
+    // ray.lookAt(mouseX, mouseY);
+
+    // let point = ray.cast(wall);
+    // if(point){
+    //     fill(255);
+    //     ellipse(point.x, point.y, 8, 8);
+    // }
+}
