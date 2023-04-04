fdf96772fb5ef560e9ab41094217228381cb0c7a
JonasPost2006 <43406465+JonasPost2006@users.noreply.github.com>
2023-04-04 18:02:19 +0200

    Raytracing werkend!

--- Raycasting test/sketch.js
+++ Raycasting test/sketch.js
@@ -1,11 +1,34 @@
+let walls = [];
+let ray;
+let particle;
 
-let b;
 function setup(){
     createCanvas(400, 400);
-    b = new Boundary(300, 100, 300, 300)
+    for(let i = 0; i < 5; i++){
+        let x1 = random(width);
+        let x2 = random(width);
+        let y1 = random(height);
+        let y2 = random(height);
+        walls[i] = new Boundary(x1, y1, x2, y2);
+    }
+    particle = new Particle();
 }
 
 function draw(){
     background(0);
-    b.show();
+    for(let wall of walls){
+        wall.show();
+    }
+    particle.update(mouseX, mouseY);
+    particle.show();
+    particle.look(walls);
+
+    // ray.show();
+    // ray.lookAt(mouseX, mouseY);
+
+    // let point = ray.cast(wall);
+    // if(point){
+    //     fill(255);
+    //     ellipse(point.x, point.y, 8, 8);
+    // }
 }
