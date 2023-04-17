9425466d73c6ff41081b570863d0006a916086e7
JonasPost <jonas.post@coderclass.nl>
2023-04-17 11:40:56 +0200

    Goede speedcontrols

--- AI learns to drive/car.js
+++ AI learns to drive/car.js
@@ -6,8 +6,11 @@ class Car {
         this.height = height;
         this.speedX = 0;
         this.speedY = 0;
+        this.speed = 0;
+        this.acceleration = 0.1;
+        this.maxSpeed = 10;
+        this.friction = 0.05;
 
-        // this.controls = new Controls();
     }
 
     draw(){
@@ -16,20 +19,56 @@ class Car {
         // ctx.fill();
         fill(0);
         rect(this.x, this.y, this.width, this.height);
+        console.log(this.speed);
         
     }
     
-    move() {
-        if (keyIsDown(UP_ARROW)) {
-          this.y -= 1; // Move up by decreasing y position
-        } else if (keyIsDown(DOWN_ARROW)) {
-          this.y += 1; // Move down by increasing y position
-        } else if (keyIsDown(RIGHT_ARROW)) {
-          this.x += 1; // Move right by increasing x position
-        } else if (keyIsDown(LEFT_ARROW)) {
-          this.x -= 1; // Move left by decreasing x position
-        }
+    // move() {
+    //     if (keyIsDown(UP_ARROW)) {
+    //       if(this.speedY <= this.maxSpeed){
+    //         this.y -= this.speedY;
+    //         this.speedY += this.acceleration;
+    //       }else if(this.speedY >= 3){
+    //         this.y -= this.speedY;
+    //       }
+    //     } else if (keyIsDown(DOWN_ARROW)) {
+    //       if(this.speedY <= this.maxSpeed / 2){
+    //         this.y += this.speedY;
+    //         this.speedY += this.acceleration;
+    //       }else if(this.speedY >= 3){
+    //         this.y += this.speedY;
+    //       }
+    //     } else if (keyIsDown(RIGHT_ARROW)) {
+    //       this.x += 1; // Move right by increasing x position
+    //     } else if (keyIsDown(LEFT_ARROW)) {
+    //       this.x -= 1; // Move left by decreasing x position
+    //     }
+    //   }
+
+    move(){
+      if(keyIsDown(UP_ARROW)){
+        this.speed += this.acceleration;
       }
+      if(keyIsDown(DOWN_ARROW)){
+        this.speed -= this.acceleration;
+      }
+      if(this.speed > this.maxSpeed){
+        this.speed = this.maxSpeed;
+      }
+      if(this.speed < -this.maxSpeed / 2){
+        this.speed = -this.maxSpeed / 2;
+      }
+      if(this.speed > 0){
+        this.speed -= this.friction;
+      }
+      if(this.speed < 0){
+        this.speed += this.friction;
+      }
+      if(Math.abs(this.speed) < this.friction){     //Hierdoor wordt speed 0 als geen toets ingedrukt is, anders is speed ~ friction
+        this.speed = 0;
+      }
+      this.y -= this.speed;
+    }
 
     setSpeed(speedX, speedY){
         this.speedX = speedX;
